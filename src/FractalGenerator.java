b0b3638ac3e878e37d4ba39c233d9b66462b7810
bastiansmn <bastian.somon@gmail.com>
2021-12-17 20:17:59 +0100

    Les fractales se génèrent bien mtn

--- src/FractalGenerator.java
+++ src/FractalGenerator.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,10 +11,14 @@ public class FractalGenerator {
     private final int MAX_ITER = 1000;
     private final int RADIUS = 2;
 
+    // Le canvas dans lequel on déssine l'image
     private final BufferedImage canvas;
+    // L'image est un carré, donc la longueur
     private final int size;
+    // La longueur du carré de dessin pour le code (ici [-1;1];[-1;1]
     private final int FRAMESIZE = 2;
 
+    // La function polynomiale
     private final ComplexFunction function;
 
     public FractalGenerator(ComplexFunction function, int size) {
@@ -22,7 +27,8 @@ public class FractalGenerator {
         this.size = size;
     }
 
-    private int divergenceIndex(Complex z0) {
+    // Calcul de l'index de divergence dans l'énoncé
+    private int divergenceIndex(Complex z0, int i, int j) {
         int ite = 0;
         Complex zn = z0;
         while (ite < MAX_ITER-1 && zn.mod() <= RADIUS) {
@@ -32,27 +38,37 @@ public class FractalGenerator {
         return ite;
     }
 
+    // Remplie le canvas
+    // renvoie this, pour permettre les appels enchaîne
+    // (ex : gen.fill().render()
+    // On pourrait donc imaginer après :
+    // gen.fill().addAntialiasing().render()
     public FractalGenerator fill() {
-        double h = (double) FRAMESIZE / this.size;
+        double h = (double) FRAMESIZE / (this.size);
         int startX =  -(FRAMESIZE / 2);
-        int startY =  (FRAMESIZE / 2);
+        int startY =  -(FRAMESIZE / 2);
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
                 int ind = divergenceIndex(
-                        Complex.of(startX + j*h, startY + i*h) // = z
+                        Complex.of(startX + j*h, startY + i*h), // = z
+                        i,
+                        j
                 );
                 // If f(z) diverge
-                int color = new Random().nextInt(255);
-                if (ind < MAX_ITER - 1) {
-                    canvas.setRGB(i, j, color);
+                if (ind == MAX_ITER - 1) {
+                    int color = new Color(255, 255, 255).getRGB();
+                    canvas.setRGB(j, i, color);
                 } else {
-                    canvas.setRGB(i, j, color);
+                    int color = new Color(255 - (255 / ind), 255 - (255 / ind), 255 - (255 / ind)).getRGB();
+                    canvas.setRGB(j, i, color);
                 }
             }
         }
         return this;
     }
 
+    // Ne renvoie pas this car fin de traitement.
+    // Se contente de créer le fichier avec le canvas.
     public void render(String filename) throws IOException {
         File f = new File(filename);
         ImageIO.write(canvas, "PNG", f);
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,26 @@ import java.util.Arrays;
 
 public class Main {
 
+    private final static Complex c1 = Complex.of(.3, .5);
+    private final static Complex c2 = Complex.of(.285, .01);
+    private final static Complex c3 = Complex.of(.038088, .9754633);
+    private final static Complex c4 = Complex.of(.285, .013);
+    private final static Complex c5 = Complex.of(.285, .01);
+    private final static Complex c6 = Complex.of(-1.476, .0);
+
+
     public static void main(String[] args) {
         FractalGenerator gen = new FractalGenerator(
                 (Complex z) -> {
                     // Equivalent à :
-                    // f(z) = c + z^2 où c=1+i
-                    return Complex.of(1, 1).add(z.pow(2));
+                    // f(z) = c + z^2 où on choisis c parmi les constantes définies ci dessus
+                    return c1.add(z.pow(2));
                 },
-                100
+                500
         );
 
         try {
-            gen.fill().render("fractal.png");
+            gen.fill().render("gen/fractal.png");
         } catch (IOException ignore) {}
 
     }
