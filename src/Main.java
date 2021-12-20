a7c8b104db855dacdce2a57d434056dd0c857896
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2021-12-20 12:52:15 +0100

    Ajout du traitement de la commandline  sans traitement des couleurs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
-import java.util.Arrays;
+import static java.lang.Integer.parseInt;
+import static java.lang.System.exit;
 
 public class Main {
 
@@ -10,21 +11,66 @@ public class Main {
     private final static Complex c5 = Complex.of(.285, .01);
     private final static Complex c6 = Complex.of(-1.476, .0);
 
-
-    public static void main(String[] args) {
-        FractalGenerator gen = new FractalGenerator(
-                (Complex z) -> {
-                    // Equivalent à :
-                    // f(z) = c + z^2 où on choisis c parmi les constantes définies ci dessus
-                    return c1.add(z.pow(2));
-                },
-                500
-        );
-
-        try {
-            gen.fill().render("gen/fractal.png");
-        } catch (IOException ignore) {}
-
+    public static void main(String[] args){
+        double re = 0.0;
+        double im = 0.0;
+        int size = 500;
+        if (args.length != 0 && args.length != 3) {
+            System.out.println(" Usage : - sans argument : génère une fractale basique en nuance de gris \n " +
+                    "- <./fractale> <Nombre complexe partie réelle> <Nombre complexe partie imaginaire> <Taille fenetre>" +
+                    "<Color>\n\n" + " Fractales génériques : " +
+                    "- réel : 0.3, imaginaire : 0.5 \n" +
+                    "- réel : 0.285, imaginaire : 0.01 \n" +
+                    "- réel : 0.038088, imaginaire : 0.9754633 \n" +
+                    "- réel : 0.285, imaginaire : 0.013 \n" +
+                    "- réel : -1.476, imaginaire : 0.0 \n");
+            exit(0);
+        }
+        if (args.length == 0) {
+            FractalGenerator gen = new FractalGenerator(
+                    (Complex z) -> {
+                        // Equivalent à :
+                        // f(z) = c + z^2 où on choisis c parmi les constantes définies ci dessus
+                        return c1.add(z.pow(2));
+                    },
+                    500
+            );
+            try {
+                gen.fill().render("fractal.png");
+            } catch (IOException ignore) {
+            }
+        } else {
+            try {
+                re = Double.parseDouble(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Modèle pour la partie réelle : 0.0 ->" +
+                        " Remplacez les 0 par des valeurs ou mettez plus de nombres");
+            }
+            try {
+                im = Double.parseDouble(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Modèle pour la partie imaginaire : 0.0 ->" +
+                        " Remplacez les 0 par des valeurs ou mettez plus de nombres");
+            }
+            try {
+                size = parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                System.out.println("Modèle pour la taille de l'écran : 00 ->" +
+                        " Remplacez les 0 par des valeurs pour avoir la taille de l'écran en pixel");
+            }
+            Complex c = Complex.of(re, im);
+            FractalGenerator gen = new FractalGenerator(
+                    (Complex z) -> {
+                        // Equivalent à :
+                        // f(z) = c + z^2 où on choisis c parmi les constantes définies ci dessus
+                        return c.add(z.pow(2));
+                    },
+                    size
+            );
+            try {
+                gen.fill().render("fractal.png");
+            } catch (IOException ignore) {
+            }
+        }
     }
-
 }
